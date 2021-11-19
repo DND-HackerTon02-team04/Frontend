bd4684a54956b47955b8f3b132f87bf2a4723180
hyewonji <60416187+hyewonji@users.noreply.github.com>
2021-11-20 00:20:36 +0900

    router

---
+++ src/components/Router.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import MainPage from "../pages/MainPage";
+
+import CustomPage from "../pages/CustomPage";
+
+import PhotosPage from "../pages/PhotosPage";
+
+import ResultPage from "../pages/ResultPage";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/photos" element={<PhotosPage />}></Route>
+        <Route path="/custom" element={<CustomPage />}></Route>
+        <Route path="/result" element={<ResultPage />}></Route>
+        <Route path="/" exact={true} element={<MainPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
